345c9ff8d223a0c97534a2b9815012f4436ae213
Harrisonward72 <harrisonward72@yahoo.com>
2026-04-19 20:17:42 -0400

    initial commit

---
+++ src/main/java/guru/springframework/spring7webapp/domain/Author.java
@@ -0,0 +1,49 @@
+package guru.springframework.spring7webapp.domain;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
